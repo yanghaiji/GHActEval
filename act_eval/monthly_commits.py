be3fc32d00f56bd24e00b059e18079d7694475b3
yanghaiji <hai.ji.yang@ibm.com>
2024-05-06 11:43:59 +0800

    添加循环等待，防止接口202

--- act_eval/monthly_commits.py
+++ act_eval/monthly_commits.py
@@ -18,24 +18,32 @@ from utils.request_utils import RequestUtil
 def get_monthly_commits(repo_name, token):
     headers = {'Authorization': f'token {token}'}
     commits_url = f"{repo_name}/stats/commit_activity"
-    response = RequestUtil('base', 'url').send_request("GET", commits_url, headers=headers)
 
-    if response.status_code == 200:
-        data = response.json()
-        commit_counts = [week['total'] for week in data]
+    while True:
+        response = RequestUtil('base', 'url').send_request("GET", commits_url, headers=headers)
 
-        # Aggregate weekly commits into monthly
-        monthly_commits = {}
-        current_date = datetime.now()
-        for i in range(12):
-            last_month = current_date - timedelta(days=30 * i)
-            month_name = last_month.strftime('%Y-%m')
-            monthly_commits[month_name] = sum(commit_counts[i * 4:(i + 1) * 4])
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            break
+        elif response.status_code == 202:
+            # Retry if status code is 202
+            continue
+        else:
+            log.error(
+                f"Failed to fetch commit activity data from GitHub API for repository: {repo_name}. Status code: {response.status_code}")
+            break
 
-        log.info("%s 完成 月份分析", repo_name)
-        return monthly_commits
-    else:
+    data = response.json()
+    commit_counts = [week['total'] for week in data]
 
-        log.error(
-            f"Failed to fetch commit activity data from GitHub API for repository: {repo_name}. Status code: {response.status_code}")
-        return None
+    # Aggregate weekly commits into monthly
+    monthly_commits = {}
+    current_date = datetime.now()
+    for i in range(12):
+        last_month = current_date - timedelta(days=30 * i)
+        month_name = last_month.strftime('%Y-%m')
+        monthly_commits[month_name] = sum(commit_counts[i * 4:(i + 1) * 4])
+
+    log.info("%s 完成 月份分析", repo_name)
+    return monthly_commits
+
